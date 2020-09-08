93aa1baa87e2f3b888c2a724ee95d6cd93ad6f94
Avijit Samanta <samantaavijit.2000@gmail.com>
2020-09-08 08:27:30 +0530

    "Else & Finally in try Exception"

--- ExceptionHandaling.py
+++ ExceptionHandaling.py
@@ -1,8 +1,24 @@
+# try:
+#     a = int(input("Enter a "))
+#     b = int(input("Enter b "))
+#     print(a / b)
+# except Exception as e:
+#     print(e)
+#
+# print("This is after try block it always run")
+
+"""
+Try Else and Finally Block
+"""
+f1 = open("avijit.txt")
 try:
-    a = int(input("Enter a "))
-    b = int(input("Enter b "))
-    print(a / b)
+    f = open("avijit.txt")
 except Exception as e:
     print(e)
+else:
+    print("This will run only if except is not running")
+finally:
+    print("Run this anyway...")
+    f1.close()
 
-print("This is after try block it always run")
+print("Important Stuff")
